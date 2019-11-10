5a4b2ad8fb6bba2759c78eb83ae00bb3d25e7691
melon <meloniusz@hotmail.com>
2019-11-10 21:55:14 +0800

    Game finito

--- fruit.js
+++ fruit.js
@@ -3,8 +3,12 @@ class Fruit {
     this.x;
     this.y;
     this.pickLocation = function() {
-      this.x = (Math.floor(Math.random() * rows - 1) + 1) * scale;
-      this.y = (Math.floor(Math.random() * columns - 1) + 1) * scale;
+      this.x = (Math.floor(Math.random() * columns - 1) + 1) * scale;
+      this.y = (Math.floor(Math.random() * rows - 1) + 1) * scale;
+    };
+    this.draw = () => {
+      ctx.fillStyle = "#4cafab";
+      ctx.fillRect(this.x, this.y, scale, scale);
     };
   }
 }
